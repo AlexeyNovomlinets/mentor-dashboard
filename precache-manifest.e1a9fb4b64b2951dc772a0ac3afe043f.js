fca45e8fb0135aaa013e379152e222adcc121f4d
Aliaksei Navamlinets <alex.novomlinets@gmail.com>
2019-02-24 14:23:00 +0300

    Updates

---
+++ precache-manifest.e1a9fb4b64b2951dc772a0ac3afe043f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1eabe0b02be32446cb2063b63b58f361",
+    "url": "/mentor-dashboard/static/media/github-logo.1eabe0b0.jpg"
+  },
+  {
+    "revision": "5357f7305c488abea30c",
+    "url": "/mentor-dashboard/static/js/runtime~main.5357f730.js"
+  },
+  {
+    "revision": "29223825a4e102e8c89d",
+    "url": "/mentor-dashboard/static/js/main.29223825.chunk.js"
+  },
+  {
+    "revision": "0793cbed584305db8420",
+    "url": "/mentor-dashboard/static/js/2.0793cbed.chunk.js"
+  },
+  {
+    "revision": "29223825a4e102e8c89d",
+    "url": "/mentor-dashboard/static/css/main.77f99fbb.chunk.css"
+  },
+  {
+    "revision": "124918928d96e14ad103f70958014b80",
+    "url": "/mentor-dashboard/index.html"
+  }
+];
